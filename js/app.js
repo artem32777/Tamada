f015d3608f0e065d93e85c9ba7931110e99df67a
xxx63124 <109870624+artem32777@users.noreply.github.com>
2023-02-01 15:48:21 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -211,6 +211,9 @@
             if (location.hash) return location.hash.replace("#", "");
         }
         let bodyLockStatus = true;
+        let bodyLockToggle = (delay = 500) => {
+            if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
+        };
         let bodyUnlock = (delay = 500) => {
             let body = document.querySelector("body");
             if (bodyLockStatus) {
@@ -223,6 +226,22 @@
                     body.style.paddingRight = "0px";
                     document.documentElement.classList.remove("lock");
                 }), delay);
+                bodyLockStatus = false;
+                setTimeout((function() {
+                    bodyLockStatus = true;
+                }), delay);
+            }
+        };
+        let bodyLock = (delay = 500) => {
+            let body = document.querySelector("body");
+            if (bodyLockStatus) {
+                let lock_padding = document.querySelectorAll("[data-lp]");
+                for (let index = 0; index < lock_padding.length; index++) {
+                    const el = lock_padding[index];
+                    el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+                }
+                body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+                document.documentElement.classList.add("lock");
                 bodyLockStatus = false;
                 setTimeout((function() {
                     bodyLockStatus = true;
@@ -260,6 +279,14 @@
             }));
         }
         document.addEventListener("DOMContentLoaded", basketAdd);
+        function menuInit() {
+            if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+                if (bodyLockStatus && e.target.closest(".icon-menu")) {
+                    bodyLockToggle();
+                    document.documentElement.classList.toggle("menu-open");
+                }
+            }));
+        }
         function menuClose() {
             bodyUnlock();
             document.documentElement.classList.remove("menu-open");
@@ -533,6 +560,7 @@
         if (document.querySelectorAll("[data-prlx-parent]")) modules_flsModules.parallax = new Parallax(document.querySelectorAll("[data-prlx-parent]"));
         window["FLS"] = true;
         isWebp();
+        menuInit();
         pageNavigation();
     })();
 })();
